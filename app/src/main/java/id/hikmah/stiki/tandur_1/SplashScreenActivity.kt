9455f998b877e16d934423e56909a16772a6a27f
doyochi <nuzululhikmah117@gmail.com>
2022-08-26 15:43:30 +0700

    add font, resource color, and splash screen

---
+++ app/src/main/java/id/hikmah/stiki/tandur_1/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package id.hikmah.stiki.tandur_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
